a3fb3e73884f12ab2490305b665c0397cb0814dd
Jung Chen <jungchen@usc.edu>
2025-03-11 00:55:53 -0700

    update tutorial

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,6 +10,19 @@ public class SceneLoader : MonoBehaviour
     [SerializeField]
     private GameObject menuCanvas;
 
+    [SerializeField]
+    private GameObject tutPage1;
+    [SerializeField]
+    private GameObject tutPage2;
+    [SerializeField]
+    private GameObject tutPage3;
+    [SerializeField]
+    private GameObject tutPage4;
+    [SerializeField]
+    private GameObject tutPage5;
+    [SerializeField]
+    private GameObject tutPage6;
+
     void Start()
     {
         menuCanvas.SetActive(true);
@@ -20,11 +33,7 @@ public class SceneLoader : MonoBehaviour
     // This function loads the Game Scene
     public void StartGame()
     {
-        SceneManager.LoadScene(1); // Loads scene by index (Game Scene)
-    }
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(1); // Loads scene by index (Game Scene)
+        SceneManager.LoadScene(1);
     }
 
     public void ShowTut1()
@@ -44,9 +53,79 @@ public class SceneLoader : MonoBehaviour
     public void ReturnMainMenu()
     {
         menuCanvas.SetActive(true);
-        tutMessage1.SetActive(false);
-        tutMessage2.SetActive(false);
+        //tutMessage1.SetActive(false);
+        //tutMessage2.SetActive(false);
+
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(false);
     }
+
+    public void ShowTutPage1()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(true);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(false);
+    }
+    public void ShowTutPage2()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(true);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(false);
+    }
+    public void ShowTutPage3()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(true);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(false);
+    }
+    public void ShowTutPage4()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(true);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(false);
+    }
+    public void ShowTutPage5()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(true);
+        tutPage6.SetActive(false);
+    }
+    public void ShowTutPage6()
+    {
+        menuCanvas.SetActive(false);
+        tutPage1.SetActive(false);
+        tutPage2.SetActive(false);
+        tutPage3.SetActive(false);
+        tutPage4.SetActive(false);
+        tutPage5.SetActive(false);
+        tutPage6.SetActive(true);
+    }
+    
+
 
 
 }
