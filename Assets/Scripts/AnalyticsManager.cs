e18a9f276fdf828058dafa06ca37046b22213406
M4ttLeong <mattleong5@gmail.com>
2025-04-10 22:15:31 -0700

    Modified the analytics manager to log in to an authenticated user to log analytics. Read access still set to anyone can read.

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -5,9 +5,30 @@ using UnityEngine.Networking;
 
 public class AnalyticsManager : MonoBehaviour
 {
+    [System.Serializable]
+    private class LoginPayload
+    {
+        public string email;
+        public string password;
+        public bool returnSecureToken = true;
+    }
+
+    [System.Serializable]
+    public class TokenOnly
+    {
+        public string idToken;
+    }
+
     //This DB is NOT locked currently
     private string firebaseURL = "https://botorbought-85297-default-rtdb.firebaseio.com/";
     public static AnalyticsManager Instance { get; private set; }
+
+    //didn't end up using the auth or user imported packages
+    
+    private string idToken = null;
+    private string email = "csci526@teamtba.com";
+    private string password = "SecurePassword213?bot";
+    private string apiKey = "AIzaSyB3ImcBHHO7FPE6wm_ISLv0q_FWHhIPVBw";
 
     private void Awake()
     {
@@ -20,11 +41,57 @@ public class AnalyticsManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
+
+        StartCoroutine(SignInAndGetToken());
+    }
+
+    private IEnumerator SignInAndGetToken()
+    {
+        //https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+        string loginUrl = $"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={apiKey}";
+
+        LoginPayload payload = new LoginPayload
+        {
+            email = this.email,
+            password = this.password,
+            returnSecureToken = true
+        };
+
+        string jsonPayload = JsonUtility.ToJson(payload);
+
+        using (UnityWebRequest request = new UnityWebRequest(loginUrl, "POST"))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonPayload);
+            using UploadHandlerRaw uploadHandler = new UploadHandlerRaw(jsonToSend);
+            request.uploadHandler = uploadHandler;
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.disposeUploadHandlerOnDispose = true;
+            request.disposeDownloadHandlerOnDispose = true;
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.timeout = 5;
+
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Firebase sign in failed!");
+                Debug.LogError($"Error: {request.error}");
+                Debug.LogError($"Response: {request.downloadHandler.text}");
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+                TokenOnly tokenResponse = JsonUtility.FromJson<TokenOnly>(json);
+                idToken = tokenResponse.idToken;
+                Debug.Log(idToken);
+                Debug.Log("Firebase sign in successful, token acquired!");
+            }
+        }
     }
 
     public void LogWorkbenchSale(List<CardData> soldBankCards, int totalPointsAwarded)
     {
-        string url = $"{firebaseURL}workbench_sales.json";
+        string url = $"{firebaseURL}workbench_sales.json?auth={idToken}";
 
         //take the carddata given and generate a name string from that
         List<string> soldCardNames = new List<string>();
@@ -41,7 +108,7 @@ public class AnalyticsManager : MonoBehaviour
 
     public void LogGameTurns(int totalTurns, int totalPassTurns, int winnerPlayerNum)
     {
-        string url = $"{firebaseURL}game_turns.json";
+        string url = $"{firebaseURL}game_turns.json?auth={idToken}";
 
         GameTurnsCounts turns = new GameTurnsCounts(totalTurns, totalPassTurns, winnerPlayerNum);
         string json = JsonUtility.ToJson(turns);
